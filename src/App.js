16a003e0a801c6d7dabc5bc099cddac371de3381
Darrick Fauvel <darrick.fauvel@gmail.com>
2022-01-13 07:12:02 -0500

    Refactor scrolling method

--- src/App.js
+++ src/App.js
@@ -12,6 +12,26 @@ function App() {
   const servicesRef = useRef()
   const featuredRef = useRef()
   const galleryRef = useRef()
+
+  function scrollToAnchor(e) {
+    e.preventDefault()
+    setShowLinks(false)
+
+    let sectionName = 'home'
+
+    if (e.target.getAttribute('href')) {
+      sectionName = e.target.getAttribute('href').slice(2)
+    }
+
+    const sectionRef = eval(`${sectionName}Ref`)
+
+    const offsetPosition = sectionRef.current.offsetTop - 62
+
+    window.scrollTo({
+      left: 0,
+      top: offsetPosition,
+      behavior: 'smooth'
+    })
   }
 
   const siteContextObject = {
