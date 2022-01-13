6439219f7c009c66b47457570521a3baa596224f
Darrick Fauvel <darrick.fauvel@gmail.com>
2022-01-13 07:14:37 -0500

    Add scrolling method from site context

--- src/components/Nav/Link.jsx
+++ src/components/Nav/Link.jsx
@@ -1,6 +1,14 @@
+import { useContext } from 'react'
+import SiteContext from '../../contexts/SiteContext'
+
 const Link = ({ name, url, linkType }) => {
+  const { scrollToAnchor } = useContext(SiteContext)
+
   return (
-    <a href={url} className={`${linkType}-link scroll-link`}>
+    <a
+      href={url}
+      className={`${linkType}-link scroll-link`}
+      onClick={scrollToAnchor}>
       {name}
     </a>
   )
